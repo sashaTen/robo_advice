ce77f3f9bf96162a9bf87a1c18e2bb3f8ff73a36
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-02-17 18:25:03 +0900

    removed  all logic  from testing to  prediction  result

--- myproject/prediction/views.py
+++ myproject/prediction/views.py
@@ -15,7 +15,7 @@ def hello_world(request):
 
 
 
-def testing(request):
+def prediction_result(request):
     vectorize_artifact = Client().get_artifact_version("9f42ba36-0287-4b1e-aaa9-46fbb54d45f4")
     vectorizer = vectorize_artifact .load()
     model_artifact = Client().get_artifact_version("62efc70b-2133-497d-94f5-cfb4ff03f4fa")
@@ -40,17 +40,10 @@ def testing(request):
 # https://raw.githubusercontent.com/sashaTen/investment_app/refs/heads/main/invest/S%26P500data/clusters_df.csv
 
 
-def  prediction_result(request):
-    vectorize_artifact = Client().get_artifact_version("9f42ba36-0287-4b1e-aaa9-46fbb54d45f4")
-    vectorizer = vectorize_artifact .load()
-    model_artifact = Client().get_artifact_version("62efc70b-2133-497d-94f5-cfb4ff03f4fa")
-    model = model_artifact.load()
-    text= 'bad news'
-    text = [text]
-    strings = scrape_latest_news()
-    prediction =  model.predict(vectorizer.transform(strings))
+def  testing(request):
     
-    return   HttpResponse( prediction )
+    
+    return   HttpResponse( 'gggg')
 
 
 
