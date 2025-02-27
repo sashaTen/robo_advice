cfabfdd44bd2a4822a4075f764a680d37871dea6
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-02-27 14:13:11 +0900

    the   sentiment now  is the    numeric  and the real   price   change  was   created   and  if it   goes  up  it is 1  and  if   down   it  is    0

--- myproject/prediction/models.py
+++ myproject/prediction/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 class NewsArticle(models.Model):
     content = models.TextField()
-    sentiment = models.CharField(max_length=50)  # e.g., 'Positive', 'Negative', 'Neutral'
-    confidence = models.FloatField()  # Store the confidence score of the prediction
+    sentiment = models.FloatField()   # e.g., 'Positive', 'Negative', 'Neutral'
+    real_price_change = models.FloatField()  # Store the confidence score of the prediction
     scraped_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+   
 
-#     python manage.py migrate
+#     python manage.py   makemigrations     migrate 
