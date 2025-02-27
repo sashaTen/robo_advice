f54713b868a0ad554a16d8b8d33a646f08f03579
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-02-27 14:02:03 +0900

    created the model  in  database    called   news   article  whee    i  save the sentiment  and  the  news   as well  the  real  change

--- myproject/prediction/models.py
+++ myproject/prediction/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsArticle(models.Model):
+    content = models.TextField()
+    sentiment = models.CharField(max_length=50)  # e.g., 'Positive', 'Negative', 'Neutral'
+    confidence = models.FloatField()  # Store the confidence score of the prediction
+    scraped_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+#     python manage.py migrate
