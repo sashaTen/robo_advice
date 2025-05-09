4e65dfe7e6aee3768d82c5b9ee0a55c5204d8f01
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-05-09 16:19:23 +0900

    removed    many  l ogics   such  save  and  left   function  for the  mode l

--- myproject/prediction/views.py
+++ myproject/prediction/views.py
@@ -18,33 +18,35 @@ def hello_world(request):
 
 
 def prediction_result(request):
-    
     vectorize_artifact = Client().get_artifact_version("9f42ba36-0287-4b1e-aaa9-46fbb54d45f4")
-    vectorizer = vectorize_artifact .load()
+    vectorizer = vectorize_artifact.load()
     model_artifact = Client().get_artifact_version("0eef78a6-cfbb-411b-b397-20fe4f07b997")
     model = model_artifact.load()
+
     if request.method == "POST":
         ticker = request.POST.get("ticker")  # Get the text input from form
-        company = request.POST.get("company")
-        strings = scrape_latest_news( ticker)
-        news =  get_stock_data(f"{ticker}:NASDAQ")
-        company_news = filter_strings(strings ,    company)
-        for  i in news :
-         company_news .append(i)
-        if company_news:
-          prediction =  model.predict(vectorizer.transform(company_news))
-          company_news_single_str   =  " ".join(company_news)
-          average_sentiment =round(sum(prediction) / len(prediction))
-          price_change =  get_price_change(ticker)
-          save_news_with_sentiment(company_news_single_str  ,  average_sentiment  ,price_change)
-          if   test_data(NewsArticle) == 1:
-               auto_retrain(3,NewsArticle)
-          return HttpResponse(f"news  found: {company_news_single_str },     the  predicted  sentiment  :     {prediction} ")  # Simple response for now
-        else   :
-          return HttpResponse('news related to stock are  not    found ') 
+       
+        company_news = request.POST.get("sentiment")  # This should be renamed if it's sentiment, not news
+
+        prediction = model.predict(vectorizer.transform([company_news]))
         
 
-    return HttpResponse("Invalid request")
+    
+
+        return HttpResponse(
+            f"found: for {   ticker} the predicted sentiment: {prediction}"
+        )
+    else:
+        return HttpResponse("News related to stock are not found")
+
+
+'''
+    save_news_with_sentiment( prediction)
+
+        if test_data(NewsArticle) == 1:
+            auto_retrain(3, NewsArticle)
+'''
+
 
 # https://raw.githubusercontent.com/sashaTen/investment_app/refs/heads/main/invest/S%26P500data/clusters_df.csv
 
